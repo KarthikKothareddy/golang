bb13c58b0bc59059224ee762833f7074ca7d6836
Karthik Kothareddy <karthikreddy.kp@gmail.com>
2020-05-31 18:19:18 -0500

    exercise 2

--- vars/exercise.go
+++ vars/exercise.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	exercise1()
+	exercise2()
 }
 
 func exercise1() {
@@ -17,4 +17,20 @@ func exercise1() {
 	fmt.Println(x)
 	fmt.Println(y)
 	fmt.Println(z)
+}
+
+var X int = 42
+var Y string = "James Bond"
+var Z bool = true
+
+func exercise2() {
+	// thse will print out 0, , false
+	// these are called ZERO VALUES
+	// fmt.Println(X)
+	// fmt.Println(Y)
+	// fmt.Println(Z)
+
+	s := fmt.Sprintf("%v%v%v", X, Y, Z)
+	fmt.Println(s)
+
 }
