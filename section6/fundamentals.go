369754126e043f5899dfafeac0b8bbb89848fec9
Karthik Kothareddy <karthikreddy.kp@gmail.com>
2020-06-03 08:08:20 -0500

    numeric types

--- section6/fundamentals.go
+++ section6/fundamentals.go
@@ -2,16 +2,36 @@ package main
 
 import "fmt"
 
+// Efficient Execution
+// Efficient Compilation
+// Ease of Programming
+
 func main() {
 	fmt.Println("This is Programming fundamentals section")
-	varBoolean()
+	// varBoolean()
+	numericTypes()
 }
-
-var x bool
 
 func varBoolean() {
 
 	a := 7
 	b := 42
 	fmt.Println(a == b, a >= b, a < b)
+}
+
+var x int
+var y float64
+
+func numericTypes() {
+
+	// integers
+	x = 10
+	// x = 1.234 -- fails
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+
+	// floating points
+	y = 10.12
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
 }
