8eb8765c8d01cbbb5b7d782c25d7eaef4fc99ab1
Karthik Kothareddy <karthikreddy.kp@gmail.com>
2020-06-06 11:37:31 -0500

    exercise 2

--- section6/exercise.go
+++ section6/exercise.go
@@ -5,15 +5,34 @@ import (
 )
 
 func main() {
-	exercise1()
+	// exercise1()
+	exercise2()
 }
 
 func exercise1() {
 
 	x := 911
 
-	fmt.Println(x)
+	fmt.Printf("%d\n", x)
 	fmt.Printf("%b\n", x)
-	fmt.Printf("%X\n", x)
+	fmt.Printf("%#x\n", x)
+
+}
+
+func exercise2() {
+
+	a := (10 == 10)
+	b := (10 <= 5)
+	c := (10 >= 1)
+	d := (1 != 2)
+	e := (10 < 5)
+	f := (10 > 5)
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
 
 }
