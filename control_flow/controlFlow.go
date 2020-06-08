db9576c7c9095099266d372f25a511db991b423f
Karthik Kothareddy <karthikreddy.kp@gmail.com>
2020-06-08 08:22:46 -0500

    control flow simple for loop

--- control_flow/controlFlow.go
+++ control_flow/controlFlow.go
@@ -1,19 +1,31 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	loops()
+	// loops()
+	loops2()
 }
 
 func loops() {
 	// there is no while loop in Go
 	// for init; condition; post{}
 
-	for i := 0; i <= 100; i++ {
-		fmt.Println("Hello, Golang")
+	for i := 0; i <= 3; i++ {
+		// fmt.Println("Hello, Golang")
+		for j := 0; j <= i; j++ {
+			fmt.Println(i, j)
+		}
+	}
+
+}
+
+func loops2() {
+
+	i := 0
+	for i < 10 {
+		fmt.Println(i)
+		i++
 	}
 
 }
