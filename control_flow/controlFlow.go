38eadadf4ba28429f495c5bfda42b136963a07f4
Karthik Kothareddy <karthikreddy.kp@gmail.com>
2020-06-08 08:29:40 -0500

    loop with break

--- control_flow/controlFlow.go
+++ control_flow/controlFlow.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func main() {
 	// loops()
-	loops2()
+	// loops2()
+	loops3()
 }
 
 func loops() {
@@ -21,9 +22,21 @@ func loops() {
 }
 
 func loops2() {
-
+	// for statement with a single condition
 	i := 0
 	for i < 10 {
+		fmt.Println(i)
+		i++
+	}
+
+}
+
+func loops3() {
+	i := 1
+	for {
+		if i > 10 {
+			break
+		}
 		fmt.Println(i)
 		i++
 	}
