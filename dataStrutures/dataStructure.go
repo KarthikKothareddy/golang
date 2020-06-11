2edc3e031fc68d9ce6d9379b794e06387ad112a1
Karthik Kothareddy <karthikreddy.kp@gmail.com>
2020-06-11 08:18:51 -0500

    appending to slices and variadic parameters

--- dataStrutures/dataStructure.go
+++ dataStrutures/dataStructure.go
@@ -6,7 +6,8 @@ import (
 
 func main() {
 	// arrays()
-	slices()
+	// slices()
+	sliceOperations()
 }
 
 // GoLang recommends usage of SLICES than ARRAYS
@@ -31,6 +32,38 @@ func slices() {
 	// x := type{values}
 	// A Slice allows you to group the values of the SAME type
 	x := []int{1, 2, 3, 4, 5}
+	fmt.Println(x)
+
+	// looping over a slice
+	fmt.Println(len(x))
+
+	// loop by using range
+	for i, v := range x {
+		fmt.Println(i, v)
+	}
+	// loop without range
+	for i := 0; i <= 4; i++ {
+		fmt.Println(i, x[i])
+	}
+
+	// slicing a slice by colon
+	// [start : end] where start is INCLUSIVE and end is EXCLUSIVE
+	fmt.Println(x[1:3])
+
+}
+
+func sliceOperations() {
+
+	x := []int{5, 6, 7, 8, 9}
+	fmt.Println(x)
+
+	// append to a slice
+	x = append(x, 11, 22, 33)
+	fmt.Println(x)
+
+	y := []int{555, 666, 777}
+	x = append(x, y...)
+
 	fmt.Println(x)
 
 }
