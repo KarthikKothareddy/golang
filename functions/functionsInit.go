f1c5bb742258e28873b5423bb5783ceeebea5412
Karthik Kothareddy <karthikreddy.kp@gmail.com>
2020-06-14 09:46:30 -0500

    multi return types

--- functions/functionsInit.go
+++ functions/functionsInit.go
@@ -5,7 +5,10 @@ import (
 )
 
 func main() {
-	functionInit("Jason")
+	// n := functionInit("Jason")
+	// fmt.Println("This is from main: ", n)
+	name, ok := multiReturn("Jason", "Bourne")
+	fmt.Println(name, ok)
 }
 
 // function SYNTAX
@@ -13,6 +16,12 @@ func main() {
 // PARAMETER - definition of functions
 // ARGUMENT - passed when calling the functions
 // EVERYTHING in Go is PASS BY VALUE
-func functionInit(s string) {
+func functionInit(s string) string {
 	fmt.Println("Hello, ", s)
+	return s
+}
+
+func multiReturn(first string, last string) (string, bool) {
+	fmt.Println(first, last)
+	return last, true
 }
